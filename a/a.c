75d88245cefff8494b2a35b6e376a331a99be51d
Andre Torrez <andre@torrez.org>
2015-10-03 16:30:33 -0700

    Using external header file.

--- a/a.c
+++ a/a.c
@@ -2,34 +2,36 @@
 #include <stdio.h>
 #include <unistd.h>                  /*  for sleep()  */
 #include <curses.h>
+#include "main.h"
 
+int main(void){
 
-int main(void) {
+	WINDOW * mainwin;
 
-    WINDOW * mainwin;
+	setupEverything(mainwin);
 
+	playGame(mainwin);
+
+	cleanupEverything(mainwin);
+
+	return EXIT_SUCCESS;
+}
+
+void playGame(WINDOW *mainwin){
+	mvaddstr(13, 33, "Hello, world!");
+	refresh();
+	sleep(3);
+}
+
+void setupEverything(WINDOW *mainwin){
 	/*  Initialize ncurses  */
-
 	if ( (mainwin = initscr()) == NULL ) {
 		fprintf(stderr, "Error initialising ncurses.\n");
 		exit(EXIT_FAILURE);
 	}
-
-
-	//Display "Hello, world!" in the centre of the
-	//screen, call refresh() to show our changes, and
-	//sleep() for a few seconds to get the full screen effect  */
-
-	mvaddstr(13, 33, "Hello, world!");
-	refresh();
-	sleep(3);
-
-
-	/*  Clean up after ourselves  */
-
+}
+void cleanupEverything(WINDOW *mainwin){
 	delwin(mainwin);
 	endwin();
 	refresh();
-
-	return EXIT_SUCCESS;
 }
